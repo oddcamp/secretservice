f53316bd4010030452d9270c8af39ebd444a05ad
Filippos Vasilakis <vasilakisfil@gmail.com>
2014-12-11 12:06:40 +0100

    beautify exception, add readme

--- lib/secretservice/error.rb
+++ lib/secretservice/error.rb
@@ -3,7 +3,16 @@ module SecretService
 
   class MissingSecrets < Error
     def initialize(keys)
-      super("Missing secrets: #{keys.join ', '}")
+      super
+      @keys = keys
+    end
+
+    def backtrace
+    end
+
+    def message
+      str = "\033[31m HEY! SecretService got you! \033[0m \n"
+      str += "\033[31m Missing secrets: #{@keys.join ', '} \033[0m"
     end
   end
 end
